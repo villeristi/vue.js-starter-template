80000a315352b5562df866032113c5337957b225
Wille <ville.ristimaki@nordsoftware.com>
2016-09-13 00:52:45 +0300

    rename webpack-alias to state

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -21,7 +21,8 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'state': path.resolve(__dirname, '../src/state')
     }
   },
 
