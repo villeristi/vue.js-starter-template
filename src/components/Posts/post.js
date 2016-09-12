e02c00b59b8bf21e8f091c80d1f660aa79477de0
Wille <ville.ristimaki@nordsoftware.com>
2016-09-13 00:55:39 +0300

    Use store in Post-component

--- src/components/Posts/post.js
+++ src/components/Posts/post.js
@@ -1,5 +1,8 @@
+/*eslint-disable no-unused-vars*/
 import Vue from 'vue';
 import { postsResource } from 'src/helpers/resources';
+import store from '../../state/store';
+import { toggleLoading } from '../../state/ui/actions';
 
 import template from './post.html';
 
@@ -9,13 +12,17 @@ export default Vue.extend({
   data() {
     return {
       post: {},
+      UI: this.$select('UI')
     };
   },
 
   route: {
     data() {
+      store.dispatch(toggleLoading(true));
       const id = this.$route.params.id;
       return postsResource.get({ id }).then((response) => {
+        store.dispatch(toggleLoading(false));
+        this.$dispatch('UIChange', this.UI);
         return this.$set('post', response.data);
       }, (errorResponse) => {
         // Handle error...
