1b423dfa7e307145a170954340d0ed384b6b81a4
Wille <ville.ristimaki@nordsoftware.com>
2016-11-08 17:30:05 +0200

    Bootstrap4 structure

--- src/components/NotFound/notFound.js
+++ src/components/NotFound/notFound.js
@@ -3,8 +3,8 @@ import Vue from 'vue';
 export default Vue.extend({
   template: `
     <div>
-        <h1 class="page-header">Not found...</h1>
-        <p class="lead">Sorry :(</p>
+        <h1 class="mb-2">Not found...</h1>
+        <p>Sorry <i class="fa fa-frown-o"></i></p>
     </div>
   `,
 });
