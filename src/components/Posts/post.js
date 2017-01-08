05cdebc435a99236bd8ea9cbd23b2da9077696c9
Wille <ville.ristimaki@nordsoftware.com>
2017-01-09 01:27:44 +0200

    Fix conflicts

--- src/components/Posts/post.js
+++ src/components/Posts/post.js
@@ -10,7 +10,7 @@ export default Vue.extend({
 
   data() {
     return {
-      posts: this.$select('posts')
+      post: {}
     };
   },
 
--- src/components/Posts/posts.js
+++ src/components/Posts/posts.js
@@ -9,7 +9,7 @@ export default Vue.extend({
 
   data() {
     return {
-      posts: this.$select('posts')
+      posts: []
     };
   },
 
