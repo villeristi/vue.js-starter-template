db01c0768196a9eeca23ae97f1f59a2e0d3241e9
Wille <ville.ristimaki@nordsoftware.com>
2017-01-17 01:24:21 +0200

    Remove redundand VueResource import from main, import VueResource from http.js instead

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+
 import Navigation from 'components/Navigation/navigation';
 import Loader from 'components/Loader/Loader';
 
 Vue.use(VueRouter);
-Vue.use(VueResource);
 
+import 'src/config/http';
 import routes from 'src/routes';
 import 'src/style.scss';
 
