e781e0c199607cdd9f495c83f0437759871e16ea
Wille <ville.ristimaki@nordsoftware.com>
2017-01-21 23:46:41 +0200

    Add createPost component & routes

---
+++ src/components/Posts/createPost.js
@@ -0,0 +1,60 @@
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
+import { postsResource } from 'src/util/resources';
+import template from './editPost.html';
+
+export default Vue.extend({
+  template,
+
+  data() {
+    return {
+      post: {},
+      message: {}
+    };
+  },
+
+  methods: {
+    handleSubmit(){
+      this.$validator.validateAll().then((success) => {
+        if (success) {
+          return this.savePost();
+        }
+
+        return this;
+      });
+    },
+
+    showMessage(message = {}, timeout = 2000){
+      this.message = message;
+      setTimeout(() => {
+        this.message = {};
+      }, timeout);
+    },
+
+    savePost(){
+      return postsResource.post('/', this.post)
+        .then((response) => {
+          this.post = response.data;
+
+          this.showMessage({
+            type: 'success',
+            text: 'Post created!'
+          });
+
+          // We need to reset the fields after successfull request
+          this.fields.reset();
+        })
+        .catch((errorResponse) => {
+          // Handle error...
+          this.showMessage({
+            type: 'danger',
+            text: errorResponse
+          });
+          console.log('API responded with:', errorResponse);
+        });
+    }
+  }
+});
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Home from 'components/Home/home';
 import Posts from 'components/Posts/posts';
 import Post from 'components/Posts/post';
+import CreatePost from 'components/Posts/createPost';
 import EditPost from 'components/Posts/editPost';
 import NotFound from 'components/NotFound/notFound';
 
@@ -12,6 +13,11 @@ const routes = [
   {
     path: '/posts',
     component: Posts
+  },
+  {
+    path: '/posts/create',
+    name: 'createPost',
+    component: CreatePost
   },
   {
     path: '/post/:id',
