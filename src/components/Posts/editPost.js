1073b08a86d6b8e02114f62c8b14d13bf9dad6eb
Wille <ville.ristimaki@nordsoftware.com>
2017-01-21 23:08:50 +0200

    Add edit-post component

---
+++ src/components/Posts/editPost.js
@@ -0,0 +1,76 @@
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
+import { postsResource } from 'src/util/resources';
+import template from './editPost.html';
+
+export default Vue.extend({
+  template,
+
+  data() {
+    return {
+      post: {},
+      message: {},
+      id: this.$route.params.id
+    };
+  },
+
+  created(){
+    this.fetchPost();
+  },
+
+  methods: {
+    handleSubmit(){
+      this.$validator.validateAll().then((success) => {
+        if (success) {
+          return this.savePost();
+        }
+
+        return this;
+      });
+    },
+
+    showMessage(message = {}, timeout = 2000){
+      this.message = message;
+      setTimeout(() => {
+        this.message = {};
+      }, timeout);
+    },
+
+    savePost(){
+      return postsResource.put(`${this.id}`, this.post)
+        .then((response) => {
+          this.post = response.data;
+
+          this.showMessage({
+            type: 'success',
+            text: 'Post updated!'
+          });
+
+          // We need to reset the fields after successfull request
+          this.fields.reset();
+        })
+        .catch((errorResponse) => {
+          // Handle error...
+          this.showMessage({
+            type: 'danger',
+            text: errorResponse
+          });
+          console.log('API responded with:', errorResponse);
+        });
+    },
+
+    fetchPost(){
+      return postsResource.get(`${this.id}`)
+        .then((response) => {
+          this.post = response.data;
+        })
+        .catch((errorResponse) => {
+          // Handle error...
+          console.log('API responded with:', errorResponse);
+        });
+    }
+  }
+});
