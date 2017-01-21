3e286a5cda1b6ac1e4e1e3661fccf057a933bc75
Wille <ville.ristimaki@nordsoftware.com>
2017-01-21 23:08:38 +0200

    Clean up post-component, add edit-link

--- src/components/Posts/post.js
+++ src/components/Posts/post.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 
 import { postsResource } from 'src/util/resources';
-import { setLoading } from 'src/util/helpers';
-
 import template from './post.html';
 
 export default Vue.extend({
@@ -21,17 +19,14 @@ export default Vue.extend({
   methods: {
     fetchPost(){
       const id = this.$route.params.id;
-      setLoading(true);
-      return postsResource.get({ id })
+
+      return postsResource.get(`${id}`)
         .then((response) => {
           this.post = response.data;
         })
         .catch((errorResponse) => {
           // Handle error...
-          console.log('API responded with:', errorResponse.status);
-        })
-        .finally(() => {
-          setLoading(false);
+          console.log('API responded with:', errorResponse);
         });
     }
   }
