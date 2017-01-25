767986bdd707a3af5670512660f387a5f6c514e8
Wille <ville.ristimaki@nordsoftware.com>
2017-01-25 18:11:20 +0200

    Add filteredPosts as computed, add animations to list

--- src/components/Posts/posts.js
+++ src/components/Posts/posts.js
@@ -3,13 +3,23 @@ import Vue from 'vue';
 import { postsResource } from 'src/util/resources';
 import template from './posts.html';
 
+const animation = 'flipInX';
+const animationDelay = 25; // in ms
+
 export default Vue.extend({
   template,
 
   data() {
     return {
+      postsFilter: '',
       posts: []
     };
+  },
+
+  computed: {
+    filteredPosts() {
+      return this.posts.filter((post) => post.title.toLowerCase().indexOf(this.postsFilter.toLowerCase()) !== -1);
+    }
   },
 
   created(){
@@ -26,6 +36,20 @@ export default Vue.extend({
           // Handle error...
           console.log('API responded with:', errorResponse);
         });
+    },
+
+    // Methods for transitions
+    handleBeforeEnter(el) {
+      el.style.opacity = 0;
+      el.classList.add('animated');
+    },
+
+    handleEnter(el) {
+      const delay = el.dataset.index * animationDelay;
+      setTimeout(() => {
+        el.style.opacity = 1;
+        el.classList.add(animation);
+      }, delay);
     }
   }
 });
